9c0b68e74249a52c37a97dd5ee324620249c4fc3
Giacomo Lawrance <GiacomoLaw@users.noreply.github.com>
2017-03-06 14:39:28 +0000

    Create counter.js

---
+++ js/counter.js
@@ -0,0 +1,10 @@
+    var clicks = 0;
+    function onClick() {
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
+        var clicks = 0;
+    function downClick() {
+        clicks -= 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
