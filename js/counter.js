691e1cef6a4b5a141abefe0b56a7d8660edd2e38
Giacomo Lawrance <GiacomoLaw@users.noreply.github.com>
2017-03-07 10:11:28 +0000

    Removed keyboard shortcuts

--- js/counter.js
+++ js/counter.js
@@ -8,23 +8,3 @@
         clicks -= 1;
         document.getElementById("clicks").innerHTML = clicks;
     };
-
-// keyboard shortcuts
-
-$(document).keypress(function(e) {
- if (e.keyCode == 87) {              // "W" key
-  var clicks = 0;
-    function onClick() {
-        clicks += 1;
-        document.getElementById("clicks").innerHTML = clicks;
- }
-});
-    
-    $(document).keypress(function(e) {
- if (e.keyCode == 83) {              // "S" key
-        var clicks = 0;
-    function downClick() {
-        clicks -= 1;
-        document.getElementById("clicks").innerHTML = clicks;
- }
-});
